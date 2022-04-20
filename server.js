d5ba00432982f61c9ea7f6d1016175bb0491ccef
Glitch (glitch-hello-node) <none>
2022-04-20 15:23:04 +0000

    ⏰👵 Checkpoint ./server.js:47243/1527 ./package.json:47243/857

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
-const path = require("path");
+const fs = require("fs");
 const fastify = require("fastify")();
-
-fastify.register(require("point-of-view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
-});
 
 fastify.get("*", function (request, reply) {
   reply
-    .status(204)
+    .status(200)
     .headers({
       "Access-Control-Allow-Origin": "*",
       "Content-Type": "text/plain",
     })
-    .view("/stellar.toml");
+    .send(fs.readFileSync("./stellar.toml", { encoding: "utf8" }));
 });
 
-// Run the server and report out to the logs
 fastify.listen(process.env.PORT, "0.0.0.0", function (err, address) {
   if (err) {
     fastify.log.error(err);
